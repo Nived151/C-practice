e04de12ee248eb88e63eab44d2be3e4d34f62f66
Nived151 <sainived1511@gmail.com>
2021-06-19 12:18:54 +0530

    update

--- arr_shift.c
+++ arr_shift.c
@@ -5,22 +5,15 @@
 
 int main() {
     int n,k;
-    printf("enter n");
     scanf("%d",&n);
-    int a[n],i,j,temp;
-    printf("enter values of array");
+    int a[n],i,temp;
     for (i=0;i<n;i++) scanf("%d",&a[i]);
-    printf("enter shift");
     scanf("%d",&k);
-    
     while(k){
     temp=a[n-1];
-    for(j=n-1;j>0;j--){
-        a[j]=a[j-1];
-    }
+    for(i=n-1;i>0;i--){
+        a[i]=a[i-1];}
     a[0]=temp;
-    k--;
-    }
+    k--;}
     for(i=0;i<n;i++) printf("%d ",a[i]);
-    return 0;
-}
+    return 0;}
