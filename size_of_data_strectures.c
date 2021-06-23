16c276f6eead659dc22ea9f240a5a07e2dc8fc04
Nived151 <sainived1511@gmail.com>
2021-06-23 21:28:56 +0530

    size of data strectures

---
+++ size_of_data_strectures.c
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+#include<stdlib.h>
+int main(){
+    int i,c,f,d;
+    i = sizeof(int);
+    c= sizeof(char);
+    f=sizeof(float);
+    d=sizeof(double);
+    printf("%d %d %d %d",i,c,f,d);
+}
